345e6ea9ca6c63549292bfa2fef9953054304aff
AngererSebastian <angerer.sebastian.merlin@gmail.com>
2022-04-09 11:50:11 +0200

    box iterator, to make it dynamic,

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,10 @@ struct Args {
 #[paw::main]
 fn main(args: Args) {
     let opt = Default::default();
-    let mut words = voran::generate_words(&opt);
+    let mut words: Box<dyn Iterator<Item = String>> = Box::new(voran::generate_words(&opt));
 
     if let Some(n) = args.number {
-        words = words.take(n);
+        words = Box::new(words.take(n));
     }
 
     words.for_each(|w| println!("{w}"))
